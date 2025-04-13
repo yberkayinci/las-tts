ebbac4e9bfbd4bbd293b139c52f553873687d7bc
Yunus Berkay İnci <berkayinci25@gmail.com>
2025-04-13 15:14:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js') 
+    }
+  });
+
+  
+  win.loadFile('index.html');
+
+  
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+
+app.on('window-all-closed', function () {
+  
+  if (process.platform !== 'darwin') app.quit();
+});
